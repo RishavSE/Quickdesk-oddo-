89da953b940f5f8816a331412fdda2fd72a547de
Rishav Kumar Singh <rishavse06@gmail.com>
2025-08-02 17:25:39 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";  // adjust path as needed
+
+
+
 import UserDashboard from "./user/UserDashboard";
 import SupportDashboard from "./support/supportdashboard";
 
@@ -58,6 +62,9 @@ const App = () => {
             />
           }
         />
+        
+      
+<Route path="/register" element={<Register />} />
         <Route
           path="/user-dashboard"
           element={
